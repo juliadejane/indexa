9a10ad3827cb21ec9ccc2dcfb3b15edcc28daa44
julia <juliadejane@gmail.com>
2025-05-16 18:44:36 -0400

    adiciona lista de contatos que organiza nomes por letra inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+import agenda from './agenda.json';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +16,24 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
     ContainerComponent,
     CabecalhoComponent,
     SeparadorComponent,
+    ContatoComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  alfabeto = 'abcdefghijklmnopqrstuvwxyz';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
+
+  filtrarPorLetra(letra: string): Contato[] {
+    return this.contatos.filter((contato) => {
+      return contato.nome.toLowerCase().startsWith(letra);
+    });
+  }
+}
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
 }
